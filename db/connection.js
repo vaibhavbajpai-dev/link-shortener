bd0db9c202a813d08e2fc9535b4e9b09a5516f5e
Vaibhav <vaibhavbajpai12@gmail.com>
2019-11-14 17:08:48 +0530

    mongodb link

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,7 @@
 const monk = require('monk');
-const connectionURL = process.env.MONGODB_URI || 'localhost/instaurl-co';
+
+const connectionURL = 'mongodb://vaibhav:abc12345@ds053597.mlab.com:53597/url-prod';
+// const connectionURL = process.env.MONGODB_URI || 'localhost/instaurl-co';
 const db = monk(connectionURL);
 
 module.exports = db;
