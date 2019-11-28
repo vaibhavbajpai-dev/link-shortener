a95b8633e14bfb5e48772a9f3986230a6d6d8bf3
vaibhavb1998 <44774694+vaibhavb1998@users.noreply.github.com>
2019-11-28 19:58:58 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,6 @@ const monk = require('monk');
 
 const connectionURL = 'mongodb://vaibhav:abc12345@ds053597.mlab.com:53597/url-prod' || 'localhost/insta-url.herokuapp.com';
 
-
-// const connectionURL = process.env.MONGODB_URI || 'localhost/instaurl-co';
 const db = monk(connectionURL);
 
 module.exports = db;
